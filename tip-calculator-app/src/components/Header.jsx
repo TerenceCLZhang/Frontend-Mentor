b52a28a38a4930cc4dac6b3030d1489403f09da2
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-05-08 21:35:18 +1200

    added h1 for screen reader

--- tip-calculator-app/src/components/Header.jsx
+++ tip-calculator-app/src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     <header className="pt-15 lg:pt-0">
       <a href="/">
         <img src={logo} alt="Splitter logo" />
+        <h1 className="sr-only">Splitter</h1>
       </a>
     </header>
   );
