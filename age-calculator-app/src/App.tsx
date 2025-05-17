68dfef34e1d34786f4735d3a7696cecfd7039287
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-05-18 11:22:47 +1200

    h1 for screen readers

--- age-calculator-app/src/App.tsx
+++ age-calculator-app/src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
 
   return (
     <main className="w-[90vw] flex flex-col gap-15 items-center justify-center bg-white px-5 py-10 rounded-3xl rounded-br-[5rem] md:rounded-br-[13rem] shadow-md md:w-[75vw] max-w-[800px] md:p-12">
-      <h1 className="hidden">Age calculator</h1>
+      <h1 className="sr-only">Age calculator</h1>
       <Form setDate={setDate} />
       <hr className="border border-grey-100 w-full" />
       <AgeContainer date={date} />
